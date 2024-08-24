c6480da3232ab32dc550f8df2b7355bee913fef6
vidya sehgal <sehgal81@uwindsor.ca>
2024-08-24 20:54:27 +0530

    commit

--- app1/main.py
+++ app1/main.py
@@ -1,9 +1,9 @@
-User_prompt = "Enter a to do:"
+User_prompt = 'Enter a to do:'
 to_do1 = input(User_prompt)
 to_do2 = input(User_prompt)
 to_do3 = input(User_prompt)
 
-todos=[to_do1,to_do2,to_do3]
+todos=[to_do1,to_do2,to_do3,"Hello"]
 
 print(todos)
 
